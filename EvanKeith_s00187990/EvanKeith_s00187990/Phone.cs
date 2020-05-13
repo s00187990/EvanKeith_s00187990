01980fed65b833ff4bf9b8b47d30a27ead17bee5
Evan Keith <46888763+s00187990@users.noreply.github.com>
2020-05-13 11:59:42 +0100

    Phone class created

---
+++ EvanKeith_s00187990/EvanKeith_s00187990/Phone.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvanKeith_s00187990
+{
+    class Phone
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string OperatingSystem { get; set; }
+        public string OS_Image { get; set; }
+        public string Phone_Image { get; set; }
+
+        public void IncreasePrice(int percentage)
+        {
+            Price += Price + (Price * (percentage / 100));
+        }
+
+
+    }
+}
